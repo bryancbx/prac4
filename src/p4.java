9de3f6fd8796be8c0c8fae65b6f88fb665376151
Student <Student@PC79>
2017-11-06 10:49:01 +0800

    This is jz testing

---
+++ src/p4.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Student
+ */
+public class p4 {
+    
+    public class RegisterForCourses{
+        private List<Course> allCourses = new ArrayList<>();
+    
+    private void initializeAllCourses(){
+      allCourses.add(new Course("BAIT3003","Agile Development",3));
+}
+    
+public RegisterForCourses(){
+initializAllcourse
+}
